84ab41aa3d38d47b74f4d88c7647f9dadc082ad8
Duynguyen162 <duynguyen162204@gmail.com>
2024-11-28 23:52:33 +0700

    end

--- bai3/index.php
+++ bai3/index.php
@@ -3,19 +3,25 @@
 $sinhvien = [];
 if (($handle = fopen("KTPM3_Danh_sach_diem_danh.csv", "r")) !== FALSE) {
     // Đọc dòng đầu tiên (tiêu đề cột)
-    $header = fgetcsv($handle, 1000, ",");
-   
-    
+    $header = array_map('trim', fgetcsv($handle, 0, ","));
+
+    // Loại bỏ BOM nếu có
+    if (substr($header[0], 0, 3) === "\xEF\xBB\xBF") {
+        $header[0] = substr($header[0], 3);
+    }
+
     // Đọc các dòng dữ liệu
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        $data[0] = strval($data[0]);  // Giả sử 'username' là cột đầu tiên
-        // Tạo mảng sinh viên từ mỗi dòng
-        $sinhvien[] = array_combine($header, $data);
+    while (($data = fgetcsv($handle, 0, ",")) !== FALSE) {
+        // Kiểm tra và khớp số cột
+        if (count($header) === count($data)) {
+            $data[0] = strval($data[0]);  // Ép kiểu username thành chuỗi
+            $sinhvien[] = array_combine($header, $data);
+        } else {
+            error_log("Dòng không hợp lệ: " . implode(",", $data));
+        }
     }
     fclose($handle);
 }
-
-// Hiển thị dữ liệu lên giao diện
 ?>
 <!DOCTYPE html>
 <html lang="vi">
@@ -31,7 +37,7 @@ if (($handle = fopen("KTPM3_Danh_sach_diem_danh.csv", "r")) !== FALSE) {
 
 <table>
     <tr>
-        
+        <th>username</th>
         <th>Password</th>
         <th>Họ</th>
         <th>Tên</th>
@@ -39,11 +45,13 @@ if (($handle = fopen("KTPM3_Danh_sach_diem_danh.csv", "r")) !== FALSE) {
         <th>Email</th>
         <th>Khóa học</th>
     </tr>
-
+    
+    
     <?php 
+        
         foreach ($sinhvien as $sv){
             echo"<tr>";
-                
+                echo"<td>{$sv['username']}</td>";
                 echo"<td>{$sv['password']}</td>";
                 echo"<td>{$sv['lastname']}</td>";
                 echo"<td>{$sv['firstname']}</td>";
